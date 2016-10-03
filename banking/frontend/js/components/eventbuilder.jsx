be5feb17f413cf720ececd9b090d8b77aa98aaef
Churin Dmitry <churin@10205.ru>
2016-10-03 19:32:32 +0300

    [up|ref] Builder to material.

--- banking/frontend/js/components/eventbuilder.jsx
+++ banking/frontend/js/components/eventbuilder.jsx
@@ -1,4 +1,20 @@
 import React from 'react'
+
+
+import MuiThemeProvider  from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin     from 'react-tap-event-plugin'
+
+import {
+  TextField,
+  DatePicker,
+  AutoComplete,
+  RaisedButton,
+  FlatButton,
+  Paper,
+  Toggle
+} from 'material-ui'
+
+
 import Edit              from './edit'
 import DropdownInput     from './DropdownInput.jsx'
 import AccessCheckbox    from './accesscheckbox.jsx'
@@ -10,6 +26,8 @@ import getToken from '../utils/token.js'
 import {dateToSimple} from '../utils/string.js'
 
 import {omit} from 'lodash/object'
+
+injectTapEventPlugin() // catching tap(click) events
 
 var eventAPI = new EventAPI(getToken())
 var accountAPI = new AccountAPI(getToken())
@@ -33,8 +51,8 @@ module.exports = React.createClass({
             tempAccounts: []
         }
     },
-    handleAuthorChange: function(account) {
-        this.setState({author: account});
+    handleAuthorChange: function(index) {
+        this.setState({author: this.state.tempAccounts[index]});
     },
     handleTitleChange: function(event){
         const val = event.target.value
@@ -63,19 +81,10 @@ module.exports = React.createClass({
         this.setState({price: val, error: maybeErrors})
     },
 
-    handleChangeFile: function(event){
-        console.log(event.target.files);
-        var Files = event.target.files;
-        for (var i = 0; i != Files.length; ++i)
-            this.state.fd.append('file' + (i+1), Files[i]);
-    },
-    handleAttachFileClick: function(){
-        $('form[name="new-event-form"] input[type="file"]').trigger('click');
-    },
-    hadnleCancelClick: function(){
+    handleCancelClick: function(){
         document.location.href = '/events/';
     },
-    hadnleCreateClick: function(){
+    handleCreateClick: function(){
 		var participants = this.refs['ParticipantsTable'].state.participants;
         participants = participants.map((p) => {
             return {account: p.account.id, parts: p.parts}
@@ -108,120 +117,44 @@ module.exports = React.createClass({
             error => console.log("Error when fetch account list in EventBuilder"))
     },
     handleChangeAccess: function(event){
-        var target = $(event.target).prop('id');
-        if (target === 'public')
-        {
-            this.setState({
-                private: false
-            });
-            $('#public').prop('checked', true);
-            $('#private').prop('checked', false);
-        }
-        else
-        {
-            this.setState({
-                private: true
-            });
-            $('#private').prop('checked', true);
-            $('#public').prop('checked', false);
-        }
+        this.setState({ private: !this.state.private });
     },
     render: function(){
         var events = ['Перевод', 'Пополнение', 'Списание'];
         var author = this.state.author.user;
         return (
-            <div className="col-md-12">
-                <div className="row">
-                    <div className="col-md-10">
-                        <h3>Новое событие</h3>
+            <MuiThemeProvider>
+                <Paper>
+                    <h3>Новое событие</h3>
+                    <div style={{marginTop:'20px'}}>
+                        <TextField floatingLabelText="Название"
+                            onChange={this.handleTitleChange}
+                            value={this.state.title}/>
+                        <TextField floatingLabelText="Цена"
+                            onChange={this.handlePriceChange}
+                            value={this.state.price}/>
+                        <DatePicker floatingLabelText="Дата события"
+                            onChange={(null_stub, date) => this.handleDateChange}
+                            value={this.state.date}/>
+                        <AutoComplete floatingLabelText="Автор" hintText="Выберите из списка"
+                            dataSource={this.state.tempAccounts.map(u => u.user.username)}
+                            searchText={this.state.author.user.username}
+                            filter={(pattern, elem) => elem.startsWith(pattern)}
+                            onFocus={ e => e.target.select() }
+                            onNewRequest={ (text, index) => this.handleAuthorChange(index) }
+                            openOnFocus={true}/>
+                        <Toggle
+                            label="Закрытое событие"
+                            defaultToggled={false}
+                            onToggle={this.handleChangeAccess} />
+
+                        <RaisedButton label="Сохранить" primary={true}
+                            onClick={ this.handleCreateClick }/>
+                        <FlatButton label="Сохранить"
+                            onClick={ this.handleCancelClick }/>
                     </div>
-                    <div className="col-md-2"></div>
-                </div>
-                <div className="row" style={{marginTop:'20px'}}>
-                    <div className="col-md-1"></div>
-                    <div className="col-md-6">
-                        <form className="form-horizontal" name="new-event-form" type="post" encType="multipart">
-                            <Edit
-                                Label="Название"
-                                Type="text"
-                                Value={this.state.title}
-                                FormName="new-event-form"
-                                Change={this.handleTitleChange}
-                                Error={this.state.error.name}/>
-                            <Edit
-                                Label="Дата"
-                                Type="date"
-                                Value={this.state.date}
-                                FormName="new-event-form"
-                                Change={this.handleDateChange}
-                                Error={this.state.error.date}/>
-                            <Edit
-                                Label="Сумма"
-                                Type="number"
-                                Value={this.state.price}
-                                FormName="new-event-form"
-                                Change={this.handlePriceChange}
-                                Error={this.state.error.price}/>
-
-                            <div className="row">
-                                <label className="control-label col-lg-3 col-sm-3 col-md-3"
-                                    form="new-event-form">Создатель</label>
-                                <div className="col-md-9">
-                                    <DropdownInput
-                                        items={this.state.tempAccounts}
-                                        onSelect={this.handleAuthorChange}
-                                        defaultValue={this.state.author}
-                                        toString={accountToString}
-                                        placeHolder="Author..."/>
-                                </div>
-
-                            </div>
-
-                            <div className="row">
-                                <ul id="access-event">
-                                    <AccessCheckbox IconClass="glyphicon glyphicon-book event-icon"
-                                        AccessId="public"
-                                        Header="Public"
-                                        Caption="Данное событие будет видно всем"
-                                        Change={this.handleChangeAccess} />
-                                    <AccessCheckbox IconClass="glyphicon glyphicon-lock event-icon"
-                                        AccessId="private"
-                                        Header="Private"
-                                        Caption="Данное событие будет видно только создателю и участникам"
-                                        Change={this.handleChangeAccess} />
-                                </ul>
-                            </div>
-
-                            <ParticipantsTable
-                                eventPrice={this.state.price}
-                                ref="ParticipantsTable"/>,
-
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <div id="files-list"></div>
-                                </div>
-                            </div>
-
-                            <div className="button-group row"
-                                style={{paddingRight:'20px'}}>
-                                <Edit Type="file" FormName="new-event-form"
-                                    Change={this.handleChangeFile}/>
-                                <Button Link="#" Class="btn btn-default"
-                                    Icon="glyphicon glyphicon-paperclip"
-                                    Caption="Прикрепить файл"
-                                    Click={this.handleAttachFileClick}/>
-                            </div>
-
-                            <div className="button-group row" style={{paddingRight:'20px'}}>
-                                <Button Link="#" Class="btn btn-danger" Id="cancel-event-button" Icon="glyphicon glyphicon-thumbs-down" Caption="Отменить" Click={this.hadnleCancelClick}/>
-                                <Button Link="#" Class="btn btn-success" Id="save-event-button" Icon="glyphicon glyphicon-thumbs-up" Caption="Создать" Click={this.hadnleCreateClick}/>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="col-md-5"></div>
-                </div>
-            </div>
+                </Paper>
+            </MuiThemeProvider>
         );
     }
 });
-

a2a1b6232a58ac2074899d0bdd3b1518a06dde56
Churin Dmitry <churin@10205.ru>
2016-12-21 19:47:43 +0300

    [fix](tests) actualize test to code

--- banking/tests/frontend/redux/participants.js
+++ banking/tests/frontend/redux/participants.js
@@ -15,17 +15,14 @@ import modules from '../helpers/defines.js'
 var actions = require(`${modules.participants}/actions.js`)
 var setError = require(`${modules.participants}/errors.js`).setError
 
-var reducer = require(`${modules.participants}/reducers.js`).participants
-
-
 describe("Participant actions", () => {
     it("should create an action to add particpant ", () => {
         const expected = { type: 'add_participant', id: 3, parts: 5.3 }
         expect(actions.addParticipant(3,5.3)).deep.equal(expected)
     })
     it("should create an action to set parts ", () => {
-        const expected = { type: 'upd_parts', id: 3, parts: 2 }
-        expect(actions.setParts(3,2)).deep.equal(expected)
+        const expected = { type: 'set_parts', parts: 2 }
+        expect(actions.setParts(2)).deep.equal(expected)
     })
     it("should create an action to delete particpant ", () => {
         const expected = { type: 'del_participant', id: 3 }
@@ -33,35 +30,33 @@ describe("Participant actions", () => {
     })
 })
 
-
+var reducer = require(`${modules.participants}/reducers.js`).participants
 describe("Participant reducers", () => {
     it("should return initial state", () => {
         reducer(undefined, {}).should.deep.equal({})
     })
 
     it("should add new participant, to participants list", () => {
-        reducer({0: 2}, actions.addParticipant()).should.deep.equal( {0: 2} )
         reducer({0: 2}, actions.addParticipant(1, 3.2))
           .should.deep.equal( {0: 2, 1: 3.2} )
 
-        // add exist - no action
+        // add exist - update
         reducer({0: 2}, actions.addParticipant(0, 3.2))
-          .should.deep.equal( {0: 2} )
-    })
-
-    it("should set parts of exist participant", () => {
-        reducer({0: 2}, actions.setParts(0, 3)).should.deep.equal( {0: 3} )
-        reducer({0: 2, 1: 3}, actions.setParts(1, 2.4))
-          .should.deep.equal( {0: 2, 1: 2.4} )
-
-        // set unexists thow error
-        expect(function reduce() {
-          reducer({0: 2, 1: 3.5}, actions.setParts(3, 8))
-        }).to.throw(setError)
+          .should.deep.equal( {0: 3.2} )
     })
 
     it("should remove participant", () => {
         reducer({0: 2}, actions.deleteParticipant(0)).should.deep.equal( {} )
         reducer({0: 2, 1: 3}, actions.deleteParticipant(1)).should.deep.equal( {0: 2} )
+    })
+})
+
+var adderReduc = require(`${modules.participants}/reducers.js`).adder
+describe("Adder reduces", function() {
+    it("should set parts", () => {
+        adderReduc({id: 0, parts: 2}, actions.setParts(3))
+            .should.deep.equal( {id : 0, parts: 3} )
+        adderReduc({id: 1, parts: 3}, actions.setParts(2.4))
+            .should.deep.equal( {id :1, parts: 2.4} )
     })
 })
